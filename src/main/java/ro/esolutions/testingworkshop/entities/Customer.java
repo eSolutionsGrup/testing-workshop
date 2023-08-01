7b15130336e490ff4dd011db2a280d1a32051881
Alex Tanasescu <133111405+bufnita08@users.noreply.github.com>
2023-08-01 09:15:17 +0300

    Update Customer.java

--- src/main/java/ro/esolutions/testingworkshop/entities/Customer.java
+++ src/main/java/ro/esolutions/testingworkshop/entities/Customer.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.INDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
